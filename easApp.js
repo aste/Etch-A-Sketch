b832412dfbc91aa1da56cb15cf54f90df2016093
Aste <aste@tuta.io>
2022-05-27 11:45:03 +0200

    Square grid with dynamic number of numbered cells

--- easApp.js
+++ easApp.js
@@ -1,15 +1,15 @@
 const gridContainer = document.getElementById("gridContainer");
 
-const makeGrid = (columns, rows) => {
+const makeGrid = (columns, rows, dimensionPx) => {
     gridContainer.style.setProperty('grid-template-columns', `repeat(${columns}, 1fr`);
     gridContainer.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr`);
-    for (i = 0; i < (rows * columns); i++) {
+    for (i = 0; i < (columns * rows); i++) {
         let gridCell = document.createElement("div");
         gridCell.innerText = (i + 1);
-        gridContainer.appendChild(gridCell).className = "grid-cell";
+        gridContainer.appendChild(gridCell).className = "gridCell";
+
     }
 }
 
-makeGrid(10, 5);
+makeGrid(5, 5, 1600);
 
-console.log(gridContainer);
