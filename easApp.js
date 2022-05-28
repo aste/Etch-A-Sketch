222cc38bbbb75adde46e5aafac0cbb73bf3e0505
Aste <aste@tuta.io>
2022-05-28 16:26:27 +0200

    Etch A Sketch with a reset button, eraser, black- and rainbow pen

--- easApp.js
+++ easApp.js
@@ -1,15 +1,45 @@
 const gridContainer = document.getElementById("gridContainer");
+const gridCell = document.querySelectorAll("gridCell");
 
-const makeGrid = (columns, rows, dimensionPx) => {
-    gridContainer.style.setProperty('grid-template-columns', `repeat(${columns}, 1fr`);
-    gridContainer.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr`);
-    for (i = 0; i < (columns * rows); i++) {
+
+const makeGrid = (columnsRows) => {
+    while (gridContainer.firstChild) gridContainer.removeChild(gridContainer.firstChild);
+    gridContainer.style.setProperty('grid-template-columns', `repeat(${columnsRows}, 1fr`);
+    gridContainer.style.setProperty('grid-template-rows', `repeat(${columnsRows}, 1fr`);
+    for (i = 0; i < (columnsRows * columnsRows); i++) {
         let gridCell = document.createElement("div");
-        gridCell.innerText = (i + 1);
         gridContainer.appendChild(gridCell).className = "gridCell";
-
     }
 }
 
-makeGrid(5, 5, 1600);
+makeGrid(16);
 
+
+const resetGrid = () => {
+    let cellCount = null;
+    while (cellCount < 8 || cellCount > 64) {
+        cellCount = parseInt(prompt("How many rows and columns would you like the grid to have? \nPick a number between 8 and 64"), 10)
+    }
+    makeGrid(cellCount);
+}
+
+
+let currentColor = 'black';
+
+
+const setColor = (color) => {
+    currentColor = color;
+}
+
+
+document.addEventListener('mouseover', e => {
+    if (e.target.matches(".gridCell")) {
+        if (currentColor === "rainbow") {
+            e.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else if (currentColor === "black") {
+            e.target.style.backgroundColor = `hsl(0, 0%, 0%)`;
+        } else if (currentColor === "eraser") {
+            e.target.style.backgroundColor = `hsl(0, 0%, 100%)`;
+        }
+    }
+})
